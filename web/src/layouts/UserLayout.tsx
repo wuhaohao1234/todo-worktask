055854a80e98a824845f62ef14181f49bb67cdb5
wuhao103 <wuhao103@fiberhome.com>
2021-02-03 16:19:22 +0800

    feat: 添加前端代码

---
+++ web/src/layouts/UserLayout.tsx
@@ -0,0 +1,59 @@
+import type { MenuDataItem } from '@ant-design/pro-layout';
+import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import type { ConnectProps } from 'umi';
+import { Link,  useIntl, connect} from 'umi';
+import React from 'react';
+import type { ConnectState } from '@/models/connect';
+import styles from './UserLayout.less';
+
+export type UserLayoutProps = {
+  breadcrumbNameMap: Record<string, MenuDataItem>;
+} & Partial<ConnectProps>;
+
+const UserLayout: React.FC<UserLayoutProps> = (props) => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { formatMessage } = useIntl();
+  const { breadcrumb } = getMenuData(routes);
+  const title = getPageTitle({
+    pathname: location.pathname,
+    formatMessage,
+    breadcrumb,
+    ...props,
+  });
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={title} />
+      </Helmet>
+
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <span className={styles.title}>工作平台</span>
+              </Link>
+            </div>
+          </div>
+          {children}
+        </div>
+        <DefaultFooter />
+      </div>
+    </HelmetProvider>
+  );
+};
+
+export default connect(({ settings }: ConnectState) => ({ ...settings }))(UserLayout);
